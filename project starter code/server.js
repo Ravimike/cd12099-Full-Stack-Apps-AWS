aa4ace3709ed8c08567d6125437cd4af9c4692ee
Ravikumar Maikandan <ravikumar.maikandan@philips.com>
2023-07-31 23:05:54 -0400

    Added Filtered image endpoint

--- project starter code/server.js
+++ project starter code/server.js
@@ -8,11 +8,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
-  
+  const port = process.env.PORT || 8089  
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+  function isValidWebURL(imageURL) {
+    var url;
+    try {
+      if(!imageURL){
+        return false;
+      }
+      url = new URL(imageURL);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === 'http:' || url.protocol === 'https:';
+  }
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -30,7 +41,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
     /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get('/filteredimage', async(req,res) => {
+    const{image_url} = req.query;
+    
+    if(!isValidWebURL(image_url)){
+      return res.status(400).json({error:`Image URL is invalid`});
+    }
+
+    try {
+      
+      const filteredPath = await filterImageFromURL(image_url);
+      if(filteredPath){
+        res.sendFile(filteredPath,(error) =>{
+          if(!error){
+            console.log(`Successfully sent file. Remove the local copy: ${filteredPath}`);
+            deleteLocalFiles(filteredPath);
+          }
+        })
+      }else{
+        return res.status(400).json({error:`Cannot find image for image url: ${image_url}` });
+      }
+      
+    } catch (exception) {
+      return res.status(500).json({error: `An internal error occured: ${exception}`});
+    }
+  })
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
